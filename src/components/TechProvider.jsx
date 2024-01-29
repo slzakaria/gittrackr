aa60bb3a5e1689cc1e9e334d0fd31350e45a42f0
Zakaria slimane. <zakaria.slimane@ext.groupepvcp.com>
2024-01-29 11:54:15 +0100

    added new techs

--- src/components/TechProvider.jsx
+++ src/components/TechProvider.jsx
@@ -4,16 +4,21 @@ import TechContext from "../utils/techContext";
 const TechProvider = ({ children }) => {
 	const [techs, setTechs] = useState([
 		{ name: "JavaScript", value: "javascript", active: true },
+		{ name: "TypeScript", value: "typescript", active: false },
 		{ name: "HTML", value: "html", active: false },
 		{ name: "CSS", value: "css", active: false },
 		{ name: "Python", value: "python", active: false },
 		{ name: "Java", value: "java", active: false },
 		{ name: "C#", value: "csharp", active: false },
+		{ name: "C++", value: "cpp", active: false },
 		{ name: "Ruby", value: "ruby", active: false },
+		{ name: "Rust", value: "rust", active: false },
 		{ name: "PHP", value: "php", active: false },
 		{ name: "Golang", value: "go", active: false },
 		{ name: "Dart", value: "dart", active: false },
 		{ name: "Swift", value: "swift", active: false },
+		{ name: "Bash", value: "bash", active: false },
+		{ name: "Shell", value: "shell", active: false },
 	]);
 
 	const getActive = (arr) => {
